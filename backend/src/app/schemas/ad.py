256eb5922622d4a743e1cb30543391b4f3c2ba5d
Mateusz Jedrzejczak <251536@edu.p.lodz.pl>
2026-04-27 21:23:29 +0200

    xd

--- backend/src/app/schemas/ad.py
+++ backend/src/app/schemas/ad.py
@@ -1,10 +1,6 @@
-from pydantic import BaseModel, ConfigDict, Field
+from pydantic import BaseModel, Field
 
 
 class ADUserRead(BaseModel):
     identity: str = Field(..., max_length=100)
-    display_name: str = Field(alias="displayName", max_length=255)
     groups: list[str]
-    department: str | None = Field(None, max_length=100)
-
-    model_config = ConfigDict(populate_by_name=True)
